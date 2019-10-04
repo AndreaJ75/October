c88e5a9ef8b2c5b5dc76aaf0c2fa5a37ab28569f
AndyJ75 <andrea.jouannin.devweb@gmail.com>
2019-10-04 14:33:51 +0200

    Projet track baby Andrea

---
+++ nourrisson-step1v1.py
@@ -0,0 +1,269 @@
+# Définition des constantes pour les garçons
+low_weights_boys = [2.6, 3.6, 4.5, 5.2, 5.8, 6.2, 6.6, 6.9, 7.2, 7.4, 7.7, 7.9, 8.1, 8.2, 8.4, 8.6, 8.8,
+8.9, 9.1, 9.3, 9.4, 9.6, 9.8, 9.9, 10.1, 10.2, 10.4, 10.5, 10.7, 10.8, 11, 11.1, 11.2, 11.4, 11.5, 11.6,
+11.8, 11.9, 12, 12.2, 12.3, 12.4, 12.5, 12.7, 12.8, 12.9, 13, 13.2, 13.3, 13.4, 13.5, 13.7, 13.8, 13.9, 14,
+14.1, 14.3, 14.4, 14.5, 14.6, 14.7]
+high_weights_boys = [4.2, 5.5, 6.8, 7.7, 8.4, 9, 9.5, 9.9, 10.3, 10.6, 10.9, 11.2, 11.5, 11.8, 12.1, 12.3, 12.6,
+12.9, 13.1, 13.4, 13.6, 13.9, 14.2, 14.4, 14.7, 14.9, 15.2, 15.4, 15.7, 15.9, 16.2, 16.4, 16.6, 16.9, 17.1,
+17.3, 17.5, 17.8, 18, 18.2, 18.4, 18.6, 18.9, 19.1, 19.3, 19.5, 19.8, 20, 20.2, 20.4, 20.7, 20.9, 21.1, 21.4,
+21.6, 21.8, 22.1, 22.3, 22.5, 22.8, 23]
+
+low_heights_boys = [46.8, 51.5, 55.1, 58.1, 60.5, 62.4, 64.1, 65.6, 67, 68.3, 69.5, 70.7, 71.8, 72.9, 74, 75,
+76, 76.9, 77.8, 78.7, 79.6, 80.4, 81.2, 82, 82.8, 82.8, 83.6, 84.3, 85, 85.7, 86.3, 87, 87.6, 88.2, 88.8, 89.4,
+90, 90.6, 91.1, 91.7, 92.2, 92.8, 93.3, 93.9, 94.4, 94.9, 95.4, 95.9, 96.4, 96.9, 97.4, 97.9, 98.4, 98.9, 99.4,
+99.9, 100.4, 100.9, 101.4, 101.9, 102.3]
+high_heights_boys = [53, 57.9, 61.7, 64.8, 67.3, 69.4, 71.1, 72.7, 74.2, 75.7, 77, 78.4, 79.7, 80.9, 82.1, 83.3,
+84.5, 85.6, 86.7, 87.8, 88.8, 89.9, 90.9, 91.9, 92.8, 93.1, 94, 94.9, 95.8, 96.7, 97.5, 98.4, 99.2, 99.9, 100.7,
+101.4, 102.2, 102.9, 103.6, 104.3, 105, 105.7, 106.4, 107, 107.7, 108.3, 109, 109.6, 110.2, 110.8, 111.5, 112.1,
+112.7, 113.3, 113.9, 114.5, 115.2, 115.8, 116.4, 117, 117.6]
+
+low_skulls_boys = [32.4, 35.4, 37.2, 38.6, 39.7, 40.6, 41.3, 42, 42.5, 42.9, 43.3, 43.7, 44, 44.2, 44.4, 44.7, 44.8,
+45, 45.2, 45.3, 45.5, 45.6, 45.8, 45.9, 46, 46.1, 46.2, 46.3, 46.5, 46.6, 46.6, 46.7, 46.8, 46.9, 47, 47.1, 47.1, 47.2,
+47.3, 47.3, 47.4, 47.4, 47.5, 47.6, 47.6, 47.7, 47.7, 47.8, 47.8, 47.9, 47.9, 47.9, 48, 48, 48.1, 48.1, 48.1, 48.2,
+48.2, 48.2, 48.3]
+high_skulls_boys = [36.6, 39.2, 41.1, 42.5, 43.6, 44.5, 45.3, 46, 46.6, 47.1, 47.5, 47.9, 48.2, 48.5, 48.7,
+49, 49.2, 49.4, 49.6, 49.7, 49.9, 50.1, 50.2, 50.3, 50.5, 50.6, 50.8, 50.9, 51, 51.1, 51.2, 51.3, 51.4, 51.5,
+51.6, 51.7, 51.8, 51.9, 52, 52, 52.1, 52.2, 52.2, 52.3, 52.4, 52.4, 52.5, 52.6, 52.6, 52.7, 52.7, 52.8, 52.8,
+52.9, 52.9, 53, 53, 53.1, 53.1, 53.2, 53.2]
+
+# Définition des constantes pour les filles
+low_weights_girls = [2.5, 3.3, 4.1, 4.7, 5.2, 5.6, 6, 6.3, 6.5, 6.8, 7, 7.2, 7.3, 7.5, 7.7, 7.9, 8.1, 8.2, 8.4,
+8.6, 8.7, 8.9, 9.1, 9.2, 9.4, 9.6, 9.8, 9.9, 10.1, 10.2, 10.4, 10.5, 10.7, 10.8, 11, 11.1, 11.3, 11.4, 11.6, 11.7,
+11.8, 12, 12.1, 12.2, 12.4, 12.5, 12.6, 12.8, 12.9, 13, 13.2, 13.3, 13.4, 13.5, 13.7, 13.8, 13.9, 14, 14.2, 14.3, 14.4]
+high_weights_girls = [4, 5.2, 6.3, 7.2, 7.9, 8.4, 8.9, 9.4, 9.7, 10.1, 10.4, 10.7, 11, 11.3, 11.5, 11.8, 12.1,
+12.3, 12.6, 12.9, 13.1, 13.4, 13.6, 13.9, 14.2, 14.4, 14.7, 15, 15.2, 15.5, 15.7, 16, 16.2, 16.5, 16.8, 17, 17.3,
+17.5, 17.8, 18, 18.3, 18.6, 18.8, 19.1, 19.3, 19.6, 19.9, 20.1, 20.4, 20.6, 20.9, 21.2, 21.4, 21.7, 22, 22.2, 22.5,
+22.7, 23, 23.3, 23.5]
+
+low_heights_girls = [46.1, 50.5, 53.7, 56.3, 58.5, 60.4, 62, 63.5, 64.9, 66.2, 67.4, 68.6, 69.8, 70.9, 72, 73,
+74, 75, 75.9, 76.9, 77.7, 78.6, 79.5, 80.3, 81.1, 81.2, 82, 82.7, 83.5, 84.2, 84.9, 85.6, 86.2, 86.9, 87.5,
+88.2, 88.8, 89.4, 90, 90.6, 91.2, 91.8, 92.4, 92.9, 93.5, 94, 94.6, 95.1, 95.6, 96.2, 96.7, 97.2, 97.7, 98.2,
+98.7, 99.2, 99.7, 100.2, 100.7, 101.1, 101.6]
+high_heights_girls = [52.2, 56.9, 60.4, 63.3, 65.7, 67.7, 69.5, 71.1, 72.6, 74.1, 75.5, 76.9, 78.3, 79.5, 80.8,
+82, 83.2, 84.4, 85.5, 86.6, 87.7, 88.7, 89.7, 90.7, 91.7, 92, 92.9, 93.8, 94.7, 95.6, 96.5, 97.3, 98.2, 99, 99.8,
+100.5, 101.3, 102.1, 102.8, 103.6, 104.3, 105, 105.7, 106.4, 107.1, 107.8, 108.5, 109.2, 109.8, 110.5, 111.1, 111.8,
+112.4, 113, 113.6, 114.3, 114.9, 115.5, 116.1, 116.7, 117.2]
+
+low_skulls_girls = [31.9, 34.6, 36.3, 37.5, 38.5, 39.3, 40.1, 40.7, 41.2, 41.6, 42, 42.4, 42.7, 42.9, 43.2, 43.4,
+43.6, 43.8, 44, 44.1, 44.3, 44.5, 44.6, 44.7, 44.9, 45, 45.2, 45.3, 45.4, 45.5, 45.6, 45.7, 45.8, 45.9, 46, 46.1,
+46.2, 46.3, 46.3, 46.4, 46.5, 46.6, 46.6, 46.7, 46.8, 46.8, 46.9, 46.9, 47, 47.1, 47.1, 47.2, 47.2, 47.3, 47.3, 47.4,
+47.4, 47.4, 47.5, 47.5, 47.6]
+high_skulls_girls = [35.8, 38.5, 40.2, 41.6, 42.7, 43.6, 44.3, 45, 45.6, 46, 46.4, 46.8, 47.1, 47.4, 47.7, 47.9,
+48.1, 48.3, 48.5, 48.7, 48.9, 49, 49.2, 49.3, 49.5, 49.6, 49.8, 49.9, 50, 50.1, 50.2, 50.4, 50.5, 50.6, 50.7, 50.7,
+50.8, 50.9, 51, 51.1, 51.2, 51.2, 51.3, 51.4, 51.4, 51.5, 51.6, 51.6, 51.7, 51.7, 51.8, 51.8, 51.9, 51.9, 52, 52,
+52.1, 52.1, 52.2, 52.2, 52.3]
+
+
+# -----------------------   Version  old   --------------------
+# def Ctrle_poids(genre,age,poids) :
+#     return "La norme de poids pour " + sexe + " de " + age + " mois " + " est situé entre " + min_poids_t 
+#     + " kg " + " et " + max_poids_t + " kg." + "Le poids de votre nourisson " + poids + " est" + Norm_char "." 
+# ------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# -------------------------DEBUT DU TRAITEMENT  --------------------------------
+#-------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------
+# Enregistrement des données saisies par l'utilisateur
+#--------------------------------------------------------
+
+# Fonction de contrôle des données saisies
+
+def Ctrle_data(var_ctrle,type) :
+
+    flag_ctrle = 0 
+    while  flag_ctrle == 0 :
+        try :
+            type(var_ctrle)
+            flag_ctrle == 1 
+            break
+        except ValueError :
+            return print(f'La valeur saisie ({var_ctrle})  doit être un nombre de type {type}')
+            SystemExit
+    
+# Modif
+# Saisi du genre et ctrle sur saisi du genre
+genre_bebe = input("Entrez le genre de votre nourisson ('g' pour garçon, 'f' pour fille) : ")
+
+nb_Rq_g = 0
+while (genre_bebe != "f" and genre_bebe != "g") and nb_Rq_g < 4 :
+    print("le genre du nourisson saisi doit être soit 'f', soit 'g'")
+    genre_bebe = input("Entrez le genre de votre nourisson ('g' pour garçon, 'f' pour fille) : ")
+    nb_Rq_g = nb_Rq_g + 1
+    if nb_Rq_g == 4 :
+        print("****---------------------------------------------------------------------------****")
+        print("Le genre saisi doit être correcte pour poursuivre !!! Arrêt du traitement")
+        print("****----------------------------------------------------------------------------****")
+        sys.exit()
+    else :
+        continue
+
+# Saisi de l'âge et contrôle sur age saisi :
+age_bebe_ctrle = input("Veuillez entrer l\'age de votre nourisson en mois (entre 0 et 60 mois): ")
+Ctrle_data(age_bebe_ctrle,int)
+age_bebe = int(age_bebe_ctrle)
+
+nb_Rq_a = 0
+while (age_bebe < 0 or age_bebe > 60) and nb_Rq_a < 4 :
+    print("l\'age du nourisson saisi doit être un entier compris entre 0 et 60 mois")
+    age_bebe = int(input("Veuillez entrer l\'age de votre nourisson en mois (entre 0 et 60 mois): "))
+    nb_Rq_a = nb_Rq_a +1
+    if nb_Rq_a == 4 :
+        print("****---------------------------------------------------------------------------****")
+        print("L\'âge du nourisson étant incorrect, vous ne pouvez poursuivre !!! Arrêt du traitement")
+        print("****---------------------------------------------------------------------------****")
+        sys.exit()
+    else : 
+        continue
+
+
+# Saisi du poids et Ctrle du poids saisi
+poids_bebe_ctrle = input("veuillez entrer le poids de votre nourisson en kg : ")
+Ctrle_data(poids_bebe_ctrle,float)
+poids_bebe = float(poids_bebe_ctrle)
+
+# saisi de la taille et Ctrle de la taille saisie
+taille_bebe_ctrle = input("veuillez entrer la taille de votre nourisson en cm : ")
+Ctrle_data(taille_bebe_ctrle,float)
+taille_bebe = float(taille_bebe_ctrle)
+
+# saisi du périmètre cranien
+perim_bebe_ctrle = input("veuillez entrer le périmètre cranien de votre nourisson en cm : ")
+Ctrle_data(perim_bebe_ctrle,float)
+perim_bebe = float(perim_bebe_ctrle)
+
+print(" ")
+
+#-------------------------------------------
+# Cacul des min et max pour chaque age
+#-------------------------------------------
+
+min_poids_g = float(low_weights_boys [age_bebe])
+min_poids_f = float(low_weights_girls[age_bebe])
+max_poids_g = float(high_weights_boys[age_bebe])
+max_poids_f = float(high_weights_girls[age_bebe]) 
+
+min_taille_g = float(low_heights_boys [age_bebe])
+min_taille_f = float(low_heights_girls[age_bebe])
+max_taille_g = float(high_heights_boys[age_bebe])
+max_taille_f = float(high_heights_girls[age_bebe]) 
+
+min_perim_g = float(low_skulls_boys [age_bebe])
+min_perim_f = float(low_skulls_girls[age_bebe])
+max_perim_g = float(high_skulls_boys[age_bebe])
+max_perim_f = float(high_skulls_girls[age_bebe]) 
+
+#-------------------------------------------------------------------------------------------------
+# Edition des messages en fonctions genre, age, poids, taille, perimètre cranien du nourisson
+#-------------------------------------------------------------------------------------------------
+
+sexe = "?" 
+max_poids_t = 9999
+max_poids_t = -9999
+
+if genre_bebe == "f" :
+    sexe = "une fille"
+    max_poids_t = max_poids_f
+    min_poids_t = min_poids_f
+    max_taille_t = max_taille_f
+    min_taille_t = min_taille_f
+    max_perim_t = max_perim_f
+    min_perim_t = min_perim_f
+    if (poids_bebe < max_poids_t and poids_bebe > min_poids_t) :
+        Norm_char_p = "dans la norme"
+    else :
+        Norm_char_p = "Hors norme"
+
+    if (taille_bebe < max_taille_t and taille_bebe > min_taille_t) :
+        Norm_char_t = "dans la norme"
+    else :
+        Norm_char_t = "Hors norme"
+
+    if (perim_bebe < max_perim_t and perim_bebe > min_perim_t) :
+        Norm_char_S = "dans la norme"
+    else :
+        Norm_char_S = "Hors norme"
+
+elif genre_bebe == "g" :
+    sexe = "pour un garçon"
+    max_poids_t = max_poids_g
+    min_poids_t = min_poids_g
+    max_taille_t = max_taille_g
+    min_taille_t = min_taille_g
+    max_perim_t = max_perim_g
+    min_perim_t = min_perim_g
+
+    if (poids_bebe < max_poids_t and poids_bebe > min_poids_t) :
+        Norm_char_p ="dans la norme"
+    else :
+        Norm_char_p = "Hors norme"
+
+    if (taille_bebe < max_taille_t and taille_bebe > min_taille_t) :
+        Norm_char_t = "dans la norme"
+    else :
+        Norm_char_t = "Hors norme"
+
+    if (perim_bebe < max_perim_t and perim_bebe > min_perim_t) :
+        Norm_char_S = "dans la norme"
+    else :
+        Norm_char_S = "Hors norme"
+
+else :
+    sexe = "indéterminé"
+    max_poids_t = 9999
+    min_poids_t = -9999
+    Norm_char_p = "Indéterminé"
+
+
+#---------------------------------------------------------------------------------------------------------
+# Message retour suite contrôle sur données saisies : genre_bebe, age_bebe, poids_bebe, permiètre_cranien_bebe
+#--------------------------------------------------------------------------------------------------------
+
+# Poids
+print (f'La norme de poids pour {sexe} de {age_bebe} mois est situé entre {min_poids_t} kg et {max_poids_t} kg. Le poids de votre nourrisson ({poids_bebe} kg) est {Norm_char_p}')
+print(" ")
+
+# Taille
+print (f'La norme de taille pour {sexe} de {age_bebe} mois est situé entre {min_taille_t} cm et {max_taille_t} cm. La taille de votre nourrisson ({taille_bebe} cm) est {Norm_char_t}')
+print(" ")
+
+# Périmètre cranien
+print (f'La norme de périmètre cranien pour {sexe} de {age_bebe} mois est situé entre {min_perim_t} cm et {max_perim_t} cm. Le périmètre cranien de votre nourrisson ({perim_bebe} cm) est {Norm_char_S}')
+print(" ")
+
+
+# ------------------------------------
+# fin _ THE END
+# ------------------------------------
+
+#-----------------------------------------------------------------------------------------
+# ---------------------------- V2 : version pour code amelioré (non fini)----------------
+#-----------------------------------------------------------------------------------------
+
+
+# def Char_bebe(genre,age,poids,taille,perim) :
+#     return "La norme de poids xxxx pour " + genre + 
+#     " de " + age + " mois " + " est situé entre "
+#     + min_poids + " kg " + " et " + max_poids + " kg."
+#     + "\n"
+#     + "Le poids de votre nourisson " + poids 
+#     + " est" + Norm_char. 
+
+# genre_bebe = input("Entrez le genre de votre nourisson ('g' pour garçon, 'f' pour fille) : ")
+
+# age_bebe = input("Veuillez entrer l\'age de votre nourisson en mois (entre 0 et 60 mois): ")
+
+# poids_bebe = input("veuillez entrer le poids de votre nourisson en kg : ")
+
+# taille_bebe = input("Veuillez entrer la taille de votre nourisson en cm : ")
+
+# perim_bebe = input("Veuillez entrer le perimètre cranien de votre nourisson en cm : ")
+
+
+# print(Char_bebe(genre_bebe, age_bebe, poids_bebe, taille_bebe, perim_bebe))
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
